8a63135b503ac0aa07e8e92d357d485868030753
hackuna885 <oliver885@hotmail.com>
2015-10-07 23:28:45 -0500

    FastClean 2015
    Sistema de Caja

---
+++ ticket.php
@@ -0,0 +1,23 @@
+<?php 
+
+header("Content-Type: text/html; Charset=UTF-8");
+
+if (isset($_POST['btn1'])) {
+	$btn = $_POST['btn1'];
+
+	if ($btn == "CANCELAR") {
+		$con = new SQLite3("data/tienda.db") or die("Problemas para conectar");
+		$eli = $con -> query("DELETE FROM prendasTMP ");
+		$con -> close();
+		
+		echo "<script> alert('Venta CANCELADA!');</script>";
+		echo "<script> window.location='index.php';</script>";
+	}
+	if ($btn == "TICKET") {
+		echo "<script> alert('TICKET');</script>";
+		echo "<script> window.location='index.php';</script>";
+	}
+}
+
+
+ ?>
